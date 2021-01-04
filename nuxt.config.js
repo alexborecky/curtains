f217652337b53880d962144dbf5c15e7827efe83
Alexandr Borecky <alexandrborecky@Alexandrs-MacBook-Pro.local>
2021-01-04 23:26:40 +0100

    spa fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/curtains.js'
+    // '@/plugins/curtains.js'
   ],
   /*
   ** Nuxt.js dev-modules
